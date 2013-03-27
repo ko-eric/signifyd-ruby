97c1f5c46635bc2640f36c0efa52b723e6d8fb56
Alex Manelis <amanelis@gmail.com>
2013-03-26 21:07:03 -0700

    Added a util method

---
+++ lib/signifyd/util.rb
@@ -0,0 +1,94 @@
+module Signifyd
+  class Util
+    def self.objects_to_ids(h)
+      case h
+      when Resource
+        h.id
+      when Hash
+        res = {}
+        h.each { |k, v| res[k] = objects_to_ids(v) unless v.nil? }
+        res
+      when Array
+        h.map { |v| objects_to_ids(v) }
+      else
+        h
+      end
+    end
+
+    def self.file_readable(file)
+      begin
+        File.open(file) { |f| }
+      rescue
+        false
+      else
+        true
+      end
+    end
+
+    def self.symbolize_names(object)
+      case object
+      when Hash
+        new = {}
+        object.each do |key, value|
+          key = (key.to_sym rescue key) || key
+          new[key] = symbolize_names(value)
+        end
+        new
+      when Array
+        object.map { |value| symbolize_names(value) }
+      else
+        object
+      end
+    end
+
+    def self.url_encode(key)
+      URI.escape(key.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+    end
+
+    def self.flatten_params(params, parent_key=nil)
+      result = []
+      params.each do |key, value|
+        calculated_key = parent_key ? "#{parent_key}[#{url_encode(key)}]" : url_encode(key)
+        if value.is_a?(Hash)
+          result += flatten_params(value, calculated_key)
+        elsif value.is_a?(Array)
+          result += flatten_params_array(value, calculated_key)
+        else
+          result << [calculated_key, value]
+        end
+      end
+      result
+    end
+
+    def self.flatten_params_array(value, calculated_key)
+      result = []
+      value.each do |elem|
+        if elem.is_a?(Hash)
+          result += flatten_params(elem, calculated_key)
+        elsif elem.is_a?(Array)
+          result += flatten_params_array(elem, calculated_key)
+        else
+          result << ["#{calculated_key}[]", elem]
+        end
+      end
+      result
+    end
+    
+    def self.encode(value, key = nil)
+      case value
+      when Hash  then value.map { |k,v| encode(v, append_key(key,k)) }.join('&')
+      when Array then value.map { |v| encode(v, "#{key}[]") }.join('&')
+      when nil   then ''
+      else            
+        "#{key}=#{CGI.escape(value.to_s)}" 
+      end
+    end
+    
+    private
+
+    def self.append_key(root_key, key)
+      root_key.nil? ? key : "#{root_key}[#{key.to_s}]"
+    end
+    
+  end
+end
