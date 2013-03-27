5be9241bd5ddc09f2ad6a858a4421e896035d07e
Alex Manelis <amanelis@gmail.com>
2013-03-27 12:03:55 -0700

    Starting on update method

---
+++ lib/signifyd/api/update.rb
@@ -0,0 +1,16 @@
+module Signifyd
+  module API
+    module Update
+      module ClassMethods
+        def create(params={}, api_key=nil)
+          raise InvalidRequestError.new('You have passed invalid parameters to Case.create') if params == {}
+          Signifyd.request(:post, self.url, params, api_key)
+        end
+      end
+
+      def self.included(base)
+        base.extend(ClassMethods)
+      end
+    end
+  end
+end
