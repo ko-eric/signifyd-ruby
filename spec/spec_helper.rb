fd2a1a58cc2fc27815ecd57175131acb8a168dc6
Alex Manelis <amanelis@gmail.com>
2013-03-26 21:04:35 -0700

    Set default API key

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,52 +5,16 @@ end
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../lib/signifyd', __FILE__)
+require File.expand_path('../fixtures/signifyd_requests', __FILE__)
+require 'faker'
 require 'webmock/rspec'
 
 PROJECT_ROOT = File.expand_path('../..', __FILE__)
 $LOAD_PATH << File.join(PROJECT_ROOT, 'lib')
 
 RSpec.configure do |config|
-  SIGNIFYD_API_KEY = '100000000001001'
-  
+  SIGNIFYD_API_KEY = '1000000000000000010001'
+    
   config.color_enabled = true
   config.tty = true
-  config.mock_with :rspec
-  
-  config.before(:each) do
-    ARGV.clear
-    $stdout.sync ||= true
-  end
-
-  # Captures the output for analysis later
-  #
-  # @example Capture `$stderr`
-  #
-  #     output = capture(:stderr) { $stderr.puts "this is captured" }
-  #
-  # @param [Symbol] stream `:stdout` or `:stderr`
-  # @yield The block to capture stdout/stderr for.
-  # @return [String] The contents of $stdout or $stderr
-  def capture(stream)
-   begin
-     stream = stream.to_s
-     eval "$#{stream} = StringIO.new"
-     yield
-     result = eval("$#{stream}").string
-   ensure
-     eval("$#{stream} = #{stream.upcase}")
-   end
-
-   result
-  end
-
-  # Silences the output stream
-  #
-  # @example Silence `$stdout`
-  #
-  #     silence(:stdout) { $stdout.puts "hi" }
-  #
-  # @param [IO] stream The stream to use such as $stderr or $stdout
-  # @return [nil]
-  alias :silence :capture
 end
