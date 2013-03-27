6f687d8511a64acb59a735b4f1ded5c9a511d056
Alex Manelis <amanelis@gmail.com>
2013-03-26 21:06:31 -0700

    Added API endpoint for create

---
+++ lib/signifyd/api/create.rb
@@ -0,0 +1,16 @@
+module Signifyd
+  module API
+    module Create
+      module ClassMethods
+        def create(params={}, api_key=nil)
+          raise InvalidRequestError.new('You have passed invalid parameters to Case.create') if params == {}
+          Signifyd.request(:post, self.url, ActiveSupport::JSON.encode(params), api_key)
+        end
+      end
+
+      def self.included(base)
+        base.extend(ClassMethods)
+      end
+    end
+  end
+end
