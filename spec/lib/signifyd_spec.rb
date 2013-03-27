4230cc8e7eec5a788b79175418f289aa98e34fe2
Alex Manelis <amanelis@gmail.com>
2013-03-27 14:07:04 -0700

    Added test for setter methods, more coming

--- spec/lib/signifyd_spec.rb
+++ spec/lib/signifyd_spec.rb
@@ -1,6 +1,44 @@
 require 'spec_helper'
 
 describe Signifyd do
+  let(:hash) { SignifydRequests.valid_case }
+  let(:json) { JSON.dump(hash) }
+  
+  context '.api_version=' do
+    context 'when setting the api version' do
+      before { Signifyd.api_key = SIGNIFYD_API_KEY }
+      after { 
+        Signifyd.api_key = nil 
+        Signifyd.api_version = '/v1' 
+      }
+      
+      subject { 
+        Signifyd.api_version = '/v2' 
+      }
+      
+      it { should_not be_nil }
+      it {
+        expect(subject).to eq('/v2')
+      }
+    end
+  end
+  
+  context '.api_version' do    
+    context 'when calling for the api_version' do
+      before { Signifyd.api_key = SIGNIFYD_API_KEY }
+      after { Signifyd.api_key = nil }
+      
+      subject {
+        Signifyd.api_version
+      }
+      
+      it { should_not be_nil }
+      it {
+        expect(subject).to eq('/v1')
+      }
+    end
+  end
+  
   context '.api_key' do
     context 'when setting an invalid API key' do
       context 'and when checking with the .configured? method' do
@@ -23,13 +61,8 @@ describe Signifyd do
     end
     
     context 'when setting a valid API key' do
-      before {
-        Signifyd.api_key = SIGNIFYD_API_KEY
-      }
-
-      after {
-        Signifyd.api_key = nil
-      }
+      before { Signifyd.api_key = SIGNIFYD_API_KEY }
+      after { Signifyd.api_key = nil }
 
       subject {
         Signifyd.configured?
@@ -40,11 +73,8 @@ describe Signifyd do
     end
   end
   
-  context '.request' do
-    context 'when calling with a vald API key' do
-      let(:hash) { SignifydRequests.valid_case }
-      let(:json) { JSON.dump(hash) }
-
+  context '.request' do    
+    context 'with a valid API key set' do
       before {
         Signifyd.api_key = SIGNIFYD_API_KEY
         
