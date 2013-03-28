792a09a3778108245ca83f2f71106c687336033d
Alex Manelis <amanelis@gmail.com>
2013-03-28 12:11:56 -0700

    changed coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,7 @@ require 'simplecov'
 SimpleCov.start do
   add_group 'Signifyd', 'lib/'
 end
-
 require 'coveralls'
-Coveralls.wear! 'rails'
 
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../lib/signifyd', __FILE__)
